ab5345a34601ee4e7409c6f2c0cf3452cd0bb931
SophiaFavro <72363654+SophiaFavro@users.noreply.github.com>
2023-02-23 12:23:25 -0800

    added enemies and end game option

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let spirit; 
+let spirit, enemies; 
 
 function setup() {
     new Canvas(windowWidth, windowHeight);
     spiritsetup();
+    enemiessetup();
 }
 
 function draw() {
     background(color(85, 194, 96)); 
     noStroke();
-
     roads();
+
     spiritMovement();
+    enemiesMovement();
+    endGame();
+}
+
+function endGame() {
+    if(spirit.collides(enemies)|| spirit.x > windowWidth){
+        spirit.visible = false;
+        enemies.remove();
+        let endColor = color(52, 52, 235);
+        fill(endColor);
+        rect(0,0,windowWidth, windowHeight);
+        fill(color(5,5,5));
+        textSize(30);
+        textAlign(CENTER);
+        text("Game Over! Reload page to play again", windowWidth/2, windowHeight/2);
+        noLoop();
+    }
 }
 
 function roads() {
@@ -24,16 +42,16 @@ function roads() {
 
 function spiritMovement() {
     if (keyCode == RIGHT_ARROW && keyIsPressed) {
-        spirit.x += 1;
+        spirit.x += 1.5;
     }
     else if (keyCode == DOWN_ARROW && keyIsPressed) {
-        spirit.y += 1;
+        spirit.y += 1.5;
     }
     else if (keyCode == LEFT_ARROW && keyIsPressed) {
-        spirit.x -= 1;
+        spirit.x -= 1.5;
     }
     else if (keyCode == UP_ARROW && keyIsPressed) {
-        spirit.y -= 1;
+        spirit.y -= 1.5;
     }
     else if (key == ' ') {
       spirit.setSpeed(0, 0);
@@ -41,13 +59,28 @@ function spiritMovement() {
     return false;
   }
 
-
+function enemiesMovement() {
+    enemies.y += 2;
+    if(enemies.y > windowHeight) {
+        enemies.y = 0;
+    }
+}
 
 function spiritsetup() {
    spirit = new Sprite(); 
-   spirit.diameter = 40;
+   spirit.diameter = 30;
    spirit.x = 25;
    spirit.y = windowHeight/2;
    spirit.shapeColor = color(121, 242, 232);
    spirit.collider = 'static';
 }
+function enemiessetup() {
+    enemies = new Group(); 
+    enemies.diameter = 40;
+    enemies.shapeColor = color(250, 2, 2);
+    enemies.y = 0;
+    while(enemies.length < windowWidth) {
+        let enemy = new enemies.Sprite();
+        enemy.x = enemies.length * 100 - 25;
+    }
+ }
